a1ebf889616b103e997fb2e01be7c7c802620f06
abelass <websolutions@mychacra.net>
2012-11-14 13:30:31 +0100

    tient compte si ht ttc

--- formulaires/prix.php
+++ formulaires/prix.php
@@ -4,21 +4,24 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 
 function formulaires_prix_charger_dist($id_objet,$objet='article'){
+    include_spip('inc/config');
 
-
+    
 	$devises_dispos =lire_config('shop/devises');
-	
+	$taxes_inclus=lire_config('shop/taxes_inclus');
+    
 	
 	// Devise par défaut si rien configuré
 	if(!$devises_dispos)$devises_dispos=array('0'=>'EUR');
 	$devises_choisis =array();	
-	$prix_choisis =array();		
-	$d=sql_select('code_devise,objet,id_objet,prix_ht,id_prix','spip_prix','id_objet='.$id_objet.' AND objet ='.sql_quote($objet));
+	$prix_choisis =array();	
+
+	$d=sql_select('*','spip_prix','id_objet='.$id_objet.' AND objet ='.sql_quote($objet));
 	
 	//établit les devises diponible moins ceux déjà utilisés
 		
 	while($row=sql_fetch($d)){
-		$devises_choisis[$row['code_devise']] = $row['code_devise'];
+		//$devises_choisis[$row['code_devise']] = $row['code_devise'];
 
 		$prix_choisis[]=$row;
 			
@@ -31,11 +34,13 @@ function formulaires_prix_charger_dist($id_objet,$objet='article'){
 	
 	$valeurs = array(
 		'prix_choisis'=>$prix_choisis,
-	
+	    'taxes_inclus'=>'',   
 		'devises'=>$devises,	
 		'code_devise'=>'',
-		'prix_ht'=>'',									
+		'prix_ht'=>$taxes_inclus,							
 		);
+    if(test_plugin_actif('shop_declinaisons'))$valeurs['id_declinaison']='';
+    $valeurs['_hidden']='<input type="hidden" name="taxes_inclus" value="'.$taxes_inclus.'">';
 	return $valeurs;			
 }
 
@@ -44,20 +49,26 @@ function formulaires_prix_verifier_dist($id_objet,$objet='article'){
 
 	foreach(array('prix','code_devise') as $obligatoire)
 	
-	if (!_request($obligatoire)) $erreurs[$obligatoire] = _T('encheres:champ_obligatoire');	
+	if (!_request($obligatoire)) $erreurs[$obligatoire] =_T('info_obligatoire');	
 		
     return $erreurs; // si c'est vide, traiter sera appele, sinon le formulaire sera resoumis
 }
 
 /*Elimination de la base de donées */
 function formulaires_prix_traiter_dist($id_objet,$objet='article'){
+    
+    $prix=_request('prix');
 	$valeurs=array(
 		'id_objet'=>$id_objet,
 		'objet'=>$objet,	
-		'prix_ht' => _request('prix'),
 		'code_devise' => _request('code_devise')
 		);
+    if($ttc=_request('taxes_inclus'))$valeurs['prix_ttc'] = _request('prix');
+    else $valeurs['prix_ht'] = _request('prix');
+    if($id_declinaison=_request('id_declinaison'))$valeurs['id_declinaison'] =$id_declinaison;
+        
 	sql_insertq('spip_prix', $valeurs);
+    return;
 }
 
 ?>
