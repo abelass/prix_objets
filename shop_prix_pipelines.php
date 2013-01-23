0e89c61a3e9a08a57d5ddfacbd675fec8decf37d
abelass <websolutions@mychacra.net>
2013-01-23 12:12:14 +0100

    afficher le formualire seuelement si un id est présent

--- shop_prix_pipelines.php
+++ shop_prix_pipelines.php
@@ -10,12 +10,11 @@ function shop_prix_affiche_milieu($flux){
     $type = $e['type'];
     $id_table_objet = $e['id_table_objet'];   
     $id = intval($flux['args'][$id_table_objet]);
-    if(in_array($type,$objets)){
-        include_spip('shop_fonctions');   
+    if(in_array($type,$objets)){  
         if ($type=='article') {
         $id_article = $flux['args']['id_article'];
-	   $rubriques_produits=rubrique_produits($id_article);
-		if($rubriques_produits){
+	   $rubriques_produits=rubrique_prix($id_article);
+		if($rubriques_produits AND $id_article){
 			$contexte = array('id_objet'=>$id_article,'objet'=>'article');
 			$contenu .= recuperer_fond('prive/objets/editer/prix', $contexte,array('ajax'=>true));
                 if ($p=strpos($flux['data'],"<!--affiche_milieu-->"))
@@ -24,7 +23,7 @@ function shop_prix_affiche_milieu($flux){
                     $flux['data'] .= $contenu;
 				    }
 		  } 
-        else{
+        elseif($id){
             $contexte = array('id_objet'=>$id,'objet'=>$type);
             $contenu .= recuperer_fond('prive/objets/editer/prix', $contexte,array('ajax'=>true));
                 if ($p=strpos($flux['data'],"<!--affiche_milieu-->"))
