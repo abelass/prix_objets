f735e31b5925896480ad3078d09b1e1bda2290a4
abelass <rainer@websimple.be>
2018-09-28 16:30:06 +0200

    formatage à rendre international

--- prix_objets_fonctions.php
+++ prix_objets_fonctions.php
@@ -323,11 +323,10 @@ function rubriques_enfant($id_parent, $rubriques = array()) {
  * @param integer $decimals
  * @return string
  */
-function filtres_prix_formater($prix, $devise = '', $decimals = 2) {
+function filtres_prix_formater($prix, $devise = '') {
 	include_spip('inc/config');
 	include_spip('inc/cookie');
 
-	$prix = number_format($prix, $decimals);
 	$config = lire_config('prix_objets');
 
 	if (!$devise) {
